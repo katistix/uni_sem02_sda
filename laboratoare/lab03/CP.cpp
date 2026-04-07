c7f075949909c81279c7ff15fc5b22d1ca51bcb1
katistix <2018.tpg@gmail.com>
2026-04-07 07:59:04 +0300

    add comments

--- laboratoare/lab03/CP.cpp
+++ laboratoare/lab03/CP.cpp
@@ -33,6 +33,9 @@ Nod* CP::cautaPozitie(TPrioritate p) const {
 }
 
 
+/// @brief Adauga un element e cu prioritatea p in coada de prioritati
+/// @param e Elementul de adaugat
+/// @param p Prioritatea elementului
 void CP::adauga(TElem e, TPrioritate p) {
 	// Best case: coada e goala: Theta(1)
 	if(this->vida()) {
@@ -76,21 +79,25 @@ void CP::adauga(TElem e, TPrioritate p) {
 	prevNode->next = newNode; // legam prevNode de newNode
 }
 
-// arunca exceptie daca coada e vida
+/// @brief Returneaza elementul cu prioritatea cea mai mare (primul element din coada)
+/// @return Elementul cu prioritatea cea mai mare
+/// @throws exception daca coada e vida
 Element CP::element() const {
+	// Theta(1)
 	if (this->vida()) {
 		throw exception();
 	}
 
 
 	return this->startNode->value;
-
-
-	// return pair <TElem, TPrioritate>  (-1, -1);       // copy constructor
-;
 }
 
+/// @brief Sterge elementul cu prioritatea cea mai mare (primul element din coada) si il returneaza
+/// @return Elementul sters
+/// @throws exception daca coada e vida
 Element CP::sterge() {
+	// Theta(1) pentru stergerea primului element, care e cel cu prioritatea cea mai mare
+
 	if (this->vida()) {
 		throw exception();
 	}
@@ -102,19 +109,23 @@ Element CP::sterge() {
 
 	delete nodeToDelete; // stergem nodul sters
 	return elem; // returnam elementul sters
-
-	// return pair <TElem, TPrioritate>  (-1, -1);
 }
 
+/// @brief Verifica daca coada de prioritati e vida
+/// @return true daca coada e vida, false altfel
 bool CP::vida() const {
+	// Theta(1) = verificam doar daca startNode e nullptr sau nu
 	if (this->startNode == nullptr) {
 		return true;
 	}
 	return false;
 }
 
-
+/// @brief Destructorul cozii de prioritati, care elibereaza memoria alocata pentru noduri
 CP::~CP() {
+	// WC -> Theta(n) = daca coada are n elemente, atunci parcurgem toata lista pentru a sterge fiecare nod
+	// BC -> Theta(1) = daca coada e goala, atunci nu intram in while
+
 	// parcurgem toata lista si stergem fiecare nod
 	Nod *currentNode = this->startNode;
 	// pana cand nu mai exista nodul next, adica pana cand currentNode devine nullptr
